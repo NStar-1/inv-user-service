7be508fac605c5b2b97bb98556c5c840d7812648
Vyacheslav Antonov <normal301293@yandex.ru>
2022-05-13 02:25:03 +0700

    upgrade Entity class

--- src/main/java/com/user/userService/entity/AppRelease.java
+++ src/main/java/com/user/userService/entity/AppRelease.java
@@ -1,29 +1,61 @@
 package com.user.userService.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
+@Table(name = "application")
 public class AppRelease {
 
-    public void setAppReleaseAppVersion(String string) {
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
-    public void setAppReleaseDataRelease(String string) {
-    }
+    @Column(name = "app_version")
+    private String app_version;
 
-    public void setAppReleaseId(String string) {
-    }
-
-    public Object getAppId() {
-        return null;
-    }
-
-    public Object getAppVersion() {
-        return null;
-    }
-
-    public Object getDataRelease() {
-        return null;
-    }
+    @Column(name = "data_release")
+    private String data_release;
     
+
+    public AppRelease() {
+    }
+
+
+    public AppRelease(int id, String app_version, String data_release) {
+        this.id = id;
+        this.app_version = app_version;
+        this.data_release = data_release;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApp_version() {
+        return this.app_version;
+    }
+
+    public void setApp_version(String app_version) {
+        this.app_version = app_version;
+    }
+
+    public String getData_release() {
+        return this.data_release;
+    }
+
+    public void setData_release(String data_release) {
+        this.data_release = data_release;
+    }
+
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE application
 (
-    app_id SERIAL NOT NULL,
+    id SERIAL NOT NULL,
     app_version varchar(10) NOT NULL,
     data_release timestamp NOT NULL,
     primary key (app_version)
